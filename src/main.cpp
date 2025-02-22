8a4e6eecf7047849ddc8ba75bc0d059bd53fbb4a
Toby James Browne <135706062+tobybrowne@users.noreply.github.com>
2025-02-22 15:44:13 +0000

    finish lab1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
+#include <STM32FreeRTOS.h>
 
 //Constants
   const uint32_t interval = 100; //Display update interval
@@ -32,8 +34,23 @@
   const int HKOW_BIT = 5;
   const int HKOE_BIT = 6;
 
-//Display driver object
+  // store inputs state
+  struct {
+    std::bitset<12> inputs;  
+    } sysState;
+
+  // stores the current step size that should be played
+  volatile uint32_t currentStepSize;
+
+  // generate step sizes for each key's note
+  const int keyFreqs [12] = {131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247};
+  uint32_t stepSizes[12];
+
+//Display driver objects
 U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
+
+// define timer object
+HardwareTimer sampleTimer(TIM1);
 
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
@@ -47,8 +64,145 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
+void setRow(uint8_t rowIdx)
+{
+  digitalWrite(REN_PIN, LOW);
+
+  bool bit0 = rowIdx & 0b001;
+  bool bit1 = rowIdx & 0b010;
+  bool bit2 = rowIdx & 0b100;
+
+  digitalWrite(RA0_PIN, bit0);
+  digitalWrite(RA1_PIN, bit1);
+  digitalWrite(RA2_PIN, bit2);
+
+  digitalWrite(REN_PIN, HIGH);
+}
+
+std::bitset<4> readCols()
+{
+  std::bitset<4> return_set;
+  return_set[0] = digitalRead(C0_PIN);
+  return_set[1] = digitalRead(C1_PIN);
+  return_set[2] = digitalRead(C2_PIN);
+  return_set[3] = digitalRead(C3_PIN);
+
+  return return_set;
+}
+
+// task to check keys pressed every 50ms
+void scanKeysTask(void * pvParameters)
+{
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while(1)
+  {
+    // ensures we sample keyboard only every 50ms
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    // ensures we only access the actual variable once per loop
+    uint32_t currentStepSize_local;
+  
+    // loop through each row
+    for(int i = 0; i <= 2; i++ )
+    {
+      setRow(i);
+      delayMicroseconds(3);
+      std::bitset<4> columns = readCols();
+  
+      int start_id = i*4;
+      for(int j = 0; j < 4; j++)
+      {
+        sysState.inputs[start_id + j] = columns[j];
+  
+        // play note if pressed
+        if(columns[j] == 0)
+        {
+          currentStepSize_local = stepSizes[start_id + j];
+        }
+      }  
+    }
+  
+    // if no keys pressed
+    if (sysState.inputs.all())
+    {
+      currentStepSize_local = 0;
+    }
+  
+    // atomic write to currentStepSize global
+    __atomic_store_n(&currentStepSize, currentStepSize_local, __ATOMIC_RELAXED);
+  } 
+}
+
+// task to update OLED display every 100ms
+void displayUpdateTask(void * pvParameters)
+{
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while(1)
+  {
+    // ensures we update screen every 100ms
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    //Update display
+    u8g2.clearBuffer();         // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    u8g2.drawStr(2,10,"Jamie You Gimp.");  // write something to the internal memory
+
+    u8g2.setCursor(2,20);
+    u8g2.print(sysState.inputs.to_ulong(), BIN);
+
+    u8g2.setCursor(2,30);
+    u8g2.print(currentStepSize);
+    u8g2.sendBuffer();          // transfer internal memory to the display
+
+    //Toggle LED
+    digitalToggle(LED_BUILTIN);
+  } 
+}
+
+// ISR that runs 22,000 times per sec
+void sampleISR()
+{
+  // phaseAcc is increased and then overflows many times a second (a wave)
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+
+  // sawtooth voltage output depending on the phase of the wave
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
+
 void setup() {
   // put your setup code here, to run once:
+
+  // compute step sizes from key frequencies
+  for (int i = 0; i < 12; ++i) {
+    stepSizes[i] = (uint32_t)(4294967296.0 * keyFreqs[i] / 22000.0);
+  }
+
+  // start key scanning thread
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+  scanKeysTask,		/* Function that implements the task */
+  "scanKeys",		/* Text name for the task */
+  64,      		/* Stack size in words, not bytes */
+  NULL,			/* Parameter passed into the task */
+  2,			/* Task priority */
+  &scanKeysHandle );	/* Pointer to store the task handle */
+
+  // start screen update thread
+  TaskHandle_t displayUpdate = NULL;
+  xTaskCreate(
+  displayUpdateTask,		/* Function that implements the task */
+  "displayUpdate",		/* Text name for the task */
+  256,      		/* Stack size in words, not bytes */
+  NULL,			/* Parameter passed into the task */
+  1,			/* Task priority */
+  &displayUpdate );	/* Pointer to store the task handle */
+
 
   //Set pin directions
   pinMode(RA0_PIN, OUTPUT);
@@ -77,26 +231,16 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  // initialise sampling interrupt (22,000 times a sec)
+  sampleTimer.setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer.attachInterrupt(sampleISR);
+  sampleTimer.resume();
+
+  // start RTOS scheduler
+  vTaskStartScheduler();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-
-  while (millis() < next);  //Wait for next interval
-
-  next += interval;
-
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
-  
+void loop() 
+{  
 }
